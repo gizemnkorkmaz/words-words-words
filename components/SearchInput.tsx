1049959725d62ed64237c9650f6972ddbaa932b9
gizemnkorkmaz <gizemkorkmaz@teknasyon.com>
2024-12-10 21:13:13 +0300

    add clear button

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -23,9 +23,15 @@ export default function SearchInput({
 
   const handleIconClick = () => {
     if (query) {
+      setQuery("");
+    } else {
       onSearch(query);
     }
-    document.querySelector('input')?.select();
+    document.querySelector("input")?.select();
+  };
+
+  const handleInputClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    (e.target as HTMLInputElement).select();
   };
 
   return (
@@ -36,6 +42,7 @@ export default function SearchInput({
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         onKeyDown={handleKeyPress}
+        onClick={handleInputClick}
         className={`p-[12px] text-sm rounded-lg w-full pr-12 pl-4 focus:outline-none transition-all duration-300 ${
           isDarkMode ? "bg-neutral-800 text-neutral-100" : "bg-neutral-100 text-black border"
         }`}
@@ -43,22 +50,37 @@ export default function SearchInput({
       <button
         onClick={handleIconClick}
         className="absolute right-6 p-2 rounded-full focus:outline-none focus:ring-2 transition-all bg-purple-400"
-        aria-label="Search"
+        aria-label={query ? "Clear search" : "Search"}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="white"
-          className="w-5 h-5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M11 17a6 6 0 100-12 6 6 0 000 12zm4.222-1.222L21 20"
-          />
-        </svg>
+        {query ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="white"
+            className="w-5 h-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M6.225 6.225a.75.75 0 011.06 0L12 10.939l4.715-4.714a.75.75 0 111.06 1.06L13.061 12l4.714 4.715a.75.75 0 01-1.06 1.06L12 13.061l-4.715 4.714a.75.75 0 01-1.06-1.06L10.939 12 6.225 7.285a.75.75 0 010-1.06z"
+              clipRule="evenodd"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="white"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11 17a6 6 0 100-12 6 6 0 000 12zm4.222-1.222L21 20"
+            />
+          </svg>
+        )}
       </button>
     </div>
   );
